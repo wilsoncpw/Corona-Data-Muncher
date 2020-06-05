001b0b98e6a16be3bd90ed6889c5edb41b00fecb
Colin Wilson <wilsoncpw@me.com>
2020-06-05 18:12:01 +0100

    Initial Commit

---
+++ Corona Data Muncher/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Corona Data Muncher
+//
+//  Created by Colin Wilson on 05/06/2020.
+//  Copyright © 2020 Colin Wilson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Corona Data Muncher/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Corona Data Muncher
+//
+//  Created by Colin Wilson on 05/06/2020.
+//  Copyright © 2020 Colin Wilson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
